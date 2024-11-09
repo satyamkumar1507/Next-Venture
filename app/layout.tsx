44a1246f63aba66777fc23459fde3be1b8d65b40
AayushBharti <iaayushbharti@gmail.com>
2024-11-09 20:04:18 +0530

    added toast form shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
 import "./globals.css"
+import "easymde/dist/easymde.min.css"
+import { Toaster } from "@/components/ui/toaster"
 
 const workSans = localFont({
   src: [
@@ -66,6 +68,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={workSans.variable}>
+        <Toaster />
         {children}
       </body>
     </html>
