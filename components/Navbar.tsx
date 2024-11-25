8696bcafcd6d30aa562c81f9ab19be9a7ee5a8b1
AayushBharti <iaayushbharti@gmail.com>
2024-11-25 19:21:51 +0530

    updated theme color and added cover image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = async () => {
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
-        <Link href="/">
+        <Link href="/" className="font-work-sans font-bold tracking-wider text-xl">
           {/* <Image src="/logo.png" alt="logo" width={144} height={30} /> */}
           Next Venture
         </Link>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
       colors: {
         primary: {
           "100": "#FFE8F0",
-          DEFAULT: "#EE2B69",
+          DEFAULT: "#4b0082",
         },
         secondary: "#FBE843",
         black: {
@@ -50,7 +50,7 @@ const config: Config = {
       boxShadow: {
         100: "2px 2px 0px 0px rgb(0, 0, 0)",
         200: "2px 2px 0px 2px rgb(0, 0, 0)",
-        300: "2px 2px 0px 2px rgb(238, 43, 105)",
+        300: "2px 2px 0px 2px #4b0082",
       },
     },
   },
