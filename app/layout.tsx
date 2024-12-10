48ad12a9eb6bfbbf98826f4b8656f9ea9327963c
AayushBharti <iaayushbharti@gmail.com>
2024-12-10 07:27:24 +0530

    added outfit font and some styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,63 +1,44 @@
-import type { Metadata } from "next"
-import localFont from "next/font/local"
 import "./globals.css"
 import "easymde/dist/easymde.min.css"
-import { Toaster } from "@/components/ui/toaster"
 
-const workSans = localFont({
-  src: [
-    {
-      path: "./fonts/WorkSans-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Thin.ttf",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-ExtraLight.ttf",
-      weight: "100",
-      style: "normal",
-    },
-  ],
-  variable: "--font-work-sans",
-})
+import type { Metadata, Viewport } from "next"
+// import localFont from 'next/font/local'
+import { Outfit } from "next/font/google"
+
+import { Toaster } from "@/components/ui/toaster"
+import { cn } from "@/lib/utils"
+
+const outfit = Outfit({ subsets: ["latin"] })
+
+// const workSans = localFont({
+//   src: [
+//     {
+//       path: './fonts/WorkSans-Black.ttf',
+//       weight: '900',
+//       style: 'normal',
+//     },
+//     {
+//       path: './fonts/WorkSans-Medium.ttf',
+//       weight: '500',
+//       style: 'normal',
+//     },
+//     {
+//       path: './fonts/WorkSans-Regular.ttf',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//   ],
+//   variable: '--font-work-sans',
+// })
 
 export const metadata: Metadata = {
-  title: "Venture Directory",
+  title: "ventures Directory",
   description: "Pitch, Vote and Grow",
+}
+
+export const viewport: Viewport = {
+  colorScheme: "dark",
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "black" }],
 }
 
 export default function RootLayout({
@@ -66,8 +47,13 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={workSans.variable}>
+    <html lang="en" className="dark">
+      <body
+        className={cn(
+          "relative h-full scroll-smooth text-white antialiased selection:bg-pink-700/20 selection:text-pink-400",
+          outfit.className,
+        )}
+      >
         <Toaster />
         {children}
       </body>
