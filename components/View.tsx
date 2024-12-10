ee1b6f6f96200cab06f955140c6ea20377685648
AayushBharti <iaayushbharti@gmail.com>
2024-12-10 07:33:25 +0530

    updated after (since now stable) and added some stylings

--- components/View.tsx
+++ components/View.tsx
@@ -1,8 +1,9 @@
-import Ping from "@/components/Ping"
+import { after } from "next/server"
+
+import Ping from "@/components/ping"
 import { client } from "@/sanity/lib/client"
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries"
 import { writeClient } from "@/sanity/lib/write-client"
-import { unstable_after as after } from "next/server"
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
@@ -14,16 +15,16 @@ const View = async ({ id }: { id: string }) => {
       await writeClient
         .patch(id)
         .set({ views: totalViews + 1 })
-        .commit()
+        .commit(),
   )
 
   return (
-    <div className="view-container">
-      <div className="absolute -top-2 -right-2">
+    <div className="fixed bottom-3 right-3 mt-5 flex items-center justify-end rounded-lg bg-violet-700/10">
+      <div className="absolute -right-2 -top-2">
         <Ping />
       </div>
 
-      <p className="view-text">
+      <p className="rounded-lg px-4 py-2 text-[16px] font-medium capitalize">
         <span className="font-black">Views: {totalViews}</span>
       </p>
     </div>
