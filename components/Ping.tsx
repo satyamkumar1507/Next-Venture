9c8973c00fbb76061842e44bf859651ef8704fab
AayushBharti <iaayushbharti@gmail.com>
2024-12-13 08:05:46 +0530

    style: updated size and color

--- components/Ping.tsx
+++ components/Ping.tsx
@@ -3,8 +3,8 @@ const Ping = () => {
     <div className="relative">
       <div className="absolute -left-4 top-1">
         <span className="flex size-[11px]">
-          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary opacity-75"></span>
-          <span className="relative inline-flex size-[11px] rounded-full bg-primary"></span>
+          <span className="absolute inline-flex size-full animate-ping rounded-full bg-purple-700 opacity-75"></span>
+          <span className="relative inline-flex size-[11px] rounded-full bg-purple-700"></span>
         </span>
       </div>
     </div>
