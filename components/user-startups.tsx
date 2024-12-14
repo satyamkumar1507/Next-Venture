57ccf533ad3216f7e873ce1c7f10174b31b3dd37
AayushBharti <iaayushbharti@gmail.com>
2024-12-15 00:42:08 +0530

    feat: introduce UserStartups component with enhanced UI

--- components/user-startups.tsx
+++ components/user-startups.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+
+import StartupCard, { StartupTypeCard } from "@/components/startup-card"
 import { client } from "@/sanity/lib/client"
 import { STARTUPS_BY_AUTHOR_QUERY } from "@/sanity/lib/queries"
-import StartupCard, { StartupTypeCard } from "@/components/StartupCard"
 
 const UserStartups = async ({ id }: { id: string }) => {
   const startups = await client.fetch(STARTUPS_BY_AUTHOR_QUERY, { id })
