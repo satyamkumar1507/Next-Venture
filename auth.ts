855d6f5017aff2adb310f58bc693e9981fdc0fa0
AayushBharti <iaayushbharti@gmail.com>
2024-12-14 08:37:08 +0530

    refactor: reorganize imports, update component configuration, and remove IDE files
    - Moved the `AUTHOR_BY_GITHUB_ID_QUERY` import to the correct position in `auth.ts`.
    - Added `iconLibrary` configuration to `components.json`.
    - Standardized quotation marks in `instrumentation.ts`.
    - Deleted unnecessary IDE configuration files from the `.idea` directory.
    - Added a new SVG file `twice.svg` to the public images directory.

--- auth.ts
+++ auth.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries"
+
 import { client } from "@/sanity/lib/client"
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries"
 import { writeClient } from "@/sanity/lib/write-client"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
